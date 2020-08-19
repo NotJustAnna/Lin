cdfbdb7e800610cd864d531f55c8e95a23fb01b6
Anna Silva <6955035+NotJustAnna@users.noreply.github.com>
2020-08-18 22:53:19 -0300

    Fix compile error

--- src/main/kotlin/io/github/cafeteriaguild/lin/rt/lib/lang/LNull.kt
+++ src/main/kotlin/io/github/cafeteriaguild/lin/rt/lib/lang/LNull.kt
@@ -1,13 +1,9 @@
 package io.github.cafeteriaguild.lin.rt.lib.lang
 
-import io.github.cafeteriaguild.lin.rt.exceptions.LinNullPointerException
 import io.github.cafeteriaguild.lin.rt.lib.LObj
-import io.github.cafeteriaguild.lin.rt.lib.nativelang.properties.Property
 
 object LNull : LObj {
-    override fun propertyOf(name: String): Property {
-        throw LinNullPointerException()
-    }
+    override fun propertyOf(name: String): Nothing? = null
 
     override fun toString() = "null"
 }
