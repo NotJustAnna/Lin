28883b6807d2ef910c8ec5a6bb2a323df1b4f2ba
Anna Silva <6955035+NotJustAnna@users.noreply.github.com>
2021-11-03 00:52:19 -0300

    Fixed dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,6 @@ version = "0.0.1"
 repositories {
     mavenCentral()
     maven { url = uri("https://maven.cafeteria.dev") }
-    jcenter()
 }
 
 kotlin {
@@ -24,41 +23,53 @@ kotlin {
         browser()
         nodejs()
     }
-    val hostOs = System.getProperty("os.name")
-    val isMingwX64 = hostOs.startsWith("Windows")
-    val nativeTarget = when {
-        hostOs == "Mac OS X" -> macosX64("native")
-        hostOs == "Linux" -> linuxX64("native")
-        isMingwX64 -> mingwX64("native")
-        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
-    }
+
+    linuxX64("linuxX64")
+    macosX64("macosX64")
+    // macosArm64("macosArm64") // TODO
+    mingwX64("mingwX64")
 
     sourceSets {
         all { languageSettings.optIn("kotlin.RequiresOptIn") }
         val commonMain by getting {
             dependencies {
-                implementation("net.notjustanna:tartar:2.2")
+                implementation("net.notjustanna:tartar:2.3")
                 implementation("com.squareup.okio:okio:3.0.0")
             }
         }
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test"))
-                implementation("net.notjustanna:kotlin-unified-platform:1.1.1")
+                implementation("net.notjustanna:kotlin-unified-platform:1.2")
             }
         }
         val jvmMain by getting
         val jvmTest by getting
-        // val commonNonJvmMain by creating {
-        //     dependsOn(commonMain)
-        // }
-        val jsMain by getting {
-            // dependsOn(commonNonJvmMain)
-        }
+        val jsMain by getting
         val jsTest by getting
-        val nativeMain by getting {
-            // dependsOn(commonNonJvmMain)
+        val nativeMain by creating {
+            dependsOn(commonMain)
         }
-        val nativeTest by getting
+        val nativeTest by creating {
+            dependsOn(nativeMain)
+        }
+        val linuxX64Main by getting {
+            dependsOn(nativeMain)
+        }
+        val linuxX64Test by getting {
+            dependsOn(nativeTest)
+        }
+        val mingwX64Main by getting {
+            dependsOn(nativeMain)
+        }
+        val mingwX64Test by getting {
+            dependsOn(nativeTest)
+        }
+        val macosX64Main by getting {
+            dependsOn(nativeMain)
+        }
+        val macosX64Test by getting {
+            dependsOn(nativeTest)
+        }
     }
 }
